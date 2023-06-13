12c5a5a04857543e67b44f8c81a5527b9a74672e
ShinEun9 <ses2201@naver.com>
2023-06-13 10:14:45 +0900

    💄 Design: TextInput 컴포넌트 색상 변수 사용으로 코드 변경

--- src/components/Input/TextInput.jsx
+++ src/components/Input/TextInput.jsx
@@ -5,27 +5,27 @@ const TextInputBox = styled.div`
   label {
     font-weight: 500;
     font-size: 12px;
-    color: #767676;
+    color: var(--sub-text-color);
   }
 
   input {
     border: 0;
     outline: none;
-    border-bottom: 1px solid #dbdbdb;
+    border-bottom: 1px solid var(--border-color);
     display: block;
     width: 100%;
     height: 32px;
   }
 
   input:focus {
-    border-color: #f26e22;
+    border-color: var(--main-color);
   }
 
   p {
     margin-top: 6px;
     font-weight: 400;
     font-size: 12px;
-    color: #eb5757;
+    color: var(--red);
   }
 `;
 
