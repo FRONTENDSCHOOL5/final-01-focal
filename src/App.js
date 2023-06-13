c50a387426b2a3b976aae37bdd711dcd5455cdc2
uussong <77879633+uussong@users.noreply.github.com>
2023-06-13 10:06:26 +0900

    ✨ Feat: 전역 스타일 및 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { GlobalStyle } from './Globalstyled';
+import Router from './routes/Router';
+
 function App() {
   return (
-    <></>
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
   );
 }
 
