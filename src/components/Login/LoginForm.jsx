694023057cb6f1b3c952417bff7e3938281cba78
ShinEun9 <ses2201@naver.com>
2023-06-13 10:07:28 +0900

    💄 Design: Login Form 컴포넌트 UI 구현

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import TextInput from '../Input/TextInput';
+
+const Form = styled.form`
+  margin-bottom: 20px;
+  & > div:first-child {
+    margin-bottom: 16px;
+  }
+
+  & > div:nth-child(2) {
+    margin-bottom: 30px;
+  }
+
+  & > button {
+    display: block;
+    margin: 0 auto;
+  }
+`;
 
 export default function LoginForm() {
   return (
-    <div>
-      <TextInput type="email" id="user-id">
+    <Form>
+      <TextInput id="user-id" type="email">
         이메일
       </TextInput>
-    </div>
+      <TextInput
+        id="password"
+        type="password"
+        // error="*비밀번호는 6자 이상이어야 합니다."
+      >
+        비밀번호
+      </TextInput>
+      <button>로그인</button>
+    </Form>
   );
 }
