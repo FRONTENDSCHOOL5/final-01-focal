df30e12425969ac3de26724b0340168681b63a9b
ShinEun9 <ses2201@naver.com>
2023-06-13 13:54:00 +0900

    💄Design: FollowingsPage UI 구현

--- src/pages/FollowingsPage.jsx
+++ src/pages/FollowingsPage.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 import UserFollowListItem from '../components/UserItem/UserFollowListItem';
-import UserSearchListItem from '../components/UserItem/UserSearchListItem';
+
+const Main = styled.main`
+  width: 100%;
+
+  & > section {
+    max-width: 358px;
+    width: calc(100% - 16px * 2);
+    margin: 24px auto;
+
+    & > ul li:not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
+`;
 
 export default function FollowingsPage() {
   const user = { accountname: 'eunsu2201', username: '은수' };
 
   return (
     <>
-      <UserFollowListItem user={user} />
-      <UserSearchListItem user={user} />
+      <header>
+        <h1>Followings</h1>
+      </header>
+      <Main>
+        <section>
+          <h2 className="a11y-hidden">팔로우 리스트</h2>
+          <ul>
+            <UserFollowListItem user={user} />
+            <UserFollowListItem user={user} />
+          </ul>
+        </section>
+      </Main>
     </>
   );
 }
