f27183221ccbcc154c7f1c50c4aa2fc8be6c3c1f
ShinEun9 <ses2201@naver.com>
2023-06-13 13:56:26 +0900

    💄Design: Followers Page UI 구현

--- src/pages/FollowersPage.jsx
+++ src/pages/FollowersPage.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+import UserFollowListItem from '../components/UserItem/UserFollowListItem';
+
+const Main = styled.main`
+  width: 100%;
+
+  & > section {
+    max-width: 358px;
+    width: calc(100% - 16px * 2);
+    margin: 24px auto;
+
+    & > ul li:not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
+`;
 
 export default function FollowersPage() {
-  return <div>FollowersPage</div>;
+  const user = { accountname: 'eunsu2201', username: '은수' };
+
+  return (
+    <>
+      <header>
+        <h1>Followers</h1>
+      </header>
+      <Main>
+        <section>
+          <h2 className="a11y-hidden">팔로우 리스트</h2>
+          <ul>
+            <UserFollowListItem user={user} />
+            <UserFollowListItem user={user} />
+          </ul>
+        </section>
+      </Main>
+    </>
+  );
 }
