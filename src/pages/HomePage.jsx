7b4d3f70df7554c5f4dd62c8f55d52922d522ebf
uussong <77879633+uussong@users.noreply.github.com>
2023-06-19 23:30:03 +0900

    💄 Design: 레이아웃 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Button from '../components/Button/Button';
 import Header from '../components/Header/Header';
 import logo from '../assets/images/basic-profile.png';
 import PostCard from '../components/Post/PostCard';
-import { useEffect, useState } from 'react';
 import authInstance from '../api/instance/authInstance';
 import NavBar from '../components/NavBar/NavBar';
 
-const Container = styled.main`
+const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  min-height: 100vh;
+`;
+
+const ContentWrapper = styled.div`
+  flex-grow: 1;
+  overflow-y: auto;
+  padding: 48px 0 60px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   align-items: center;
   width: 100%;
-  /* height: calc(100vh - 108px); */
+  padding: 16px;
   gap: 20px;
-  padding: 60px 16px 72px;
-  /* overflow-y: scroll; */
 `;
 
 const Img = styled.img`
@@ -49,24 +58,26 @@ export default function HomePage() {
   }, [status]);
 
   return (
-    <>
+    <PageWrapper>
       <Header type="main" />
-      <Container>
-        <h2 className="a11y-hidden">Focal 홈 피드</h2>
+      <ContentWrapper>
+        <Container>
+          <h2 className="a11y-hidden">Focal 홈 피드</h2>
 
-        {postDatas ? (
-          postDatas.map((data) => <PostCard key={data._id} data={data} />)
-        ) : (
-          <>
-            <Img src={logo} alt="감귤 마켓 로고" />
-            <Info>유저를 검색해 팔로우 해보세요!</Info>
-            <Button type="button" className="md">
-              검색하기
-            </Button>
-          </>
-        )}
-      </Container>
+          {postDatas ? (
+            postDatas.map((data) => <PostCard key={data.id} data={data} />)
+          ) : (
+            <>
+              <Img src={logo} alt="감귤 마켓 로고" />
+              <Info>유저를 검색해 팔로우 해보세요!</Info>
+              <Button type="button" className="md">
+                검색하기
+              </Button>
+            </>
+          )}
+        </Container>
+      </ContentWrapper>
       <NavBar />
-    </>
+    </PageWrapper>
   );
 }
