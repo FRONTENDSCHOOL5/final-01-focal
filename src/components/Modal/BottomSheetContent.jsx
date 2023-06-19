21aae49ac8a01c03a51ed27c0d9ff23de53daa56
uussong <77879633+uussong@users.noreply.github.com>
2023-06-19 14:43:58 +0900

    💄 Design: 버튼 폰트 사이즈 수정

--- src/components/Modal/BottomSheetContent.jsx
+++ src/components/Modal/BottomSheetContent.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const BottomSheetItem = styled.li`
-  font-size: 14px;
   padding: 12px 0;
   cursor: pointer;
+
+  button {
+    font-size: 14px;
+  }
 `;
 
 export default function BottomSheetContent({ onClick, children }) {
