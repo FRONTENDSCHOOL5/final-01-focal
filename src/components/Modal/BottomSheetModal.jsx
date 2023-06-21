c5613e7d2f6542c5414370c41f9f9c04c424e866
kimmingyo8 <kimmingyo8@naver.com>
2023-06-22 03:01:20 +0900

    ✨ Feat: 바텀시트 수정

--- src/components/Modal/BottomSheetModal.jsx
+++ src/components/Modal/BottomSheetModal.jsx
@@ -6,7 +6,7 @@ const BottomSheetSection = styled.section`
   inset: 0;
   width: 100%;
   background-color: rgba(0, 0, 0, 0.3);
-  /* z-index: -100; */
+  z-index: 100;
 `;
 
 const BottomSheet = styled.ul`
