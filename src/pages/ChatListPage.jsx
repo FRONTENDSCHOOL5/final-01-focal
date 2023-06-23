3fe21a0bb62b68d0729a59f5b8b0779f03e8342a
ShinEun9 <ses2201@naver.com>
2023-06-23 16:52:00 +0900

    💄 Design: ChatListPage header text 추가

--- src/pages/ChatListPage.jsx
+++ src/pages/ChatListPage.jsx
@@ -6,7 +6,7 @@ import NavBar from '../components/NavBar/NavBar';
 export default function ChatListPage() {
   return (
     <>
-      <Header type="basic" backBtnShow={false} />
+      <Header type="basic" backBtnShow={false} headerText={'내 채팅방'} />
       <ChatList />
       <NavBar />
     </>
