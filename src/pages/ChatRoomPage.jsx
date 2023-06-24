780fe9d325ad7029cb524c681607800feb139efe
vacation <vacationl0706@gmail.com>
2023-06-24 23:57:32 +0900

    ✨ Feat: 채팅방 나가기 바텀시트 및 모달 구현

--- src/pages/ChatRoomPage.jsx
+++ src/pages/ChatRoomPage.jsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import ChatRoom from '../components/Chat/ChatRoom';
 import TextInputBox from '../components/Input/TextInputBox';
 import authInstance from '../api/instance/authInstance';
+import useModal from '../hooks/useModal';
+import BottomSheetModal from '../components/Modal/BottomSheetModal';
+import BottomSheetContent from '../components/Modal/BottomSheetContent';
+import ConfirmModal from '../components/Modal/ConfirmModal';
 
 export default function ChatRoomPage() {
   const { _id } = useParams();
   const [user, setUser] = useState(null);
+  const {
+    isMenuOpen,
+    isModalOpen,
+    openMenu,
+    closeMenu,
+    openModal,
+    closeModal,
+  } = useModal();
+  const navigate = useNavigate();
 
   const getUserInfo = async () => {
     try {
@@ -27,10 +40,31 @@ export default function ChatRoomPage() {
 
   return (
     <>
-      <Header type="basic" ellipsisBtnShow={true} headerText={user?.username} />
+      <Header
+        type="basic"
+        ellipsisBtnShow={true}
+        headerText={user?.username}
+        onClick={() => openMenu()}
+      />
       <h2 className="a11y-hidden">대화창</h2>
       <ChatRoom data={user?.username} />
       <TextInputBox type="chat" />
+      {isMenuOpen && (
+        <BottomSheetModal setIsMenuOpen={closeMenu}>
+          <BottomSheetContent onClick={() => openModal()}>
+            채팅방 나가기
+          </BottomSheetContent>
+        </BottomSheetModal>
+      )}
+      {isModalOpen && (
+        <ConfirmModal
+          title="채팅방을 나가시겠어요?"
+          confirmInfo="나가기"
+          setIsMenuOpen={closeMenu}
+          setIsModalOpen={closeModal}
+          onClick={() => navigate('/chat')}
+        />
+      )}
     </>
   );
 }
