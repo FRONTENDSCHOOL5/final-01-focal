e2113c40401e7fc56d8e7d08a95defd7afc2d898
vacation <vacationl0706@gmail.com>
2023-06-26 01:07:12 +0900

    ✨ Feat: 댓글 엔터로도 입력 가능하게 구현

--- src/components/Input/TextInputBox.jsx
+++ src/components/Input/TextInputBox.jsx
@@ -76,6 +76,12 @@ export default function TextInputBox({ type, onButtonClick }) {
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                handleButtonClick(e.target.value);
+              }
+            }}
             placeholder="댓글 입력하기..."
             autoComplete="off"
           />
