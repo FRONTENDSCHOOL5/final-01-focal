b7299d530d52a01c24322acca4496f5e20aa90ce
ShinEun9 <ses2201@naver.com>
2023-07-27 15:03:14 +0900

    ♻️ Refactor: 여러개 이미지주소 불러오는 api 호출 모듈화

--- src/api/apis/image.js
+++ src/api/apis/image.js
@@ -15,3 +15,19 @@ export const getImageSrc = async (file) => {
     return new Promise(() => {});
   }
 };
+
+export const getMultiImageSrc = async (files) => {
+  let formData = new FormData();
+  for (let i = 0; i < files.length; i++) {
+    formData.append('image', files[i]);
+  }
+
+  try {
+    const { data } = await baseInstance.post('/image/uploadfiles', formData);
+    return data;
+  } catch (err) {
+    console.log(err);
+    alert('서버에러입니다. 잠시후 시도해주세요');
+    return new Promise(() => {});
+  }
+};
